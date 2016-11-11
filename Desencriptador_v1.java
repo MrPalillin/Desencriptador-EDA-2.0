f5fe12907a67ae45dbe334e66dcb3356988f0a25
MrPalillin <daniel.vicente@alumnos.uva.es>
2016-11-11 16:59:51 +0100

    Desencriptador(con arbol de sufijos)

--- Desencriptador_v1.java
+++ Desencriptador_v1.java
@@ -1,11 +1,72 @@
-import java.io.*;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
  * Created by dandevi on 26/10/16.
  * Created by dandevi on 26/10/16.
  */
+
+
 public class Desencriptador_v1 {
+
+    private int valor;
+    private Desencriptador_v1 padre;
+    private Desencriptador_v1 raiz;
+    private ArrayList<Desencriptador_v1> hijos;
+
+
+    /**
+     * Definición de atributos para la creación de los árboles de sufijos
+     *
+     * @param "valor" Contenido de una hoja específica
+     * @param "padre" Antecesor del elemento actual
+     * @param "hijos" Descendientes del elemento actual
+     * @param "raiz"  Primer elemento del árbol
+     */
+    public Desencriptador_v1() {
+        this.valor = valor;
+        this.padre = padre;
+        this.hijos = hijos;
+        this.raiz = raiz;
+    }
+
+    public void setValor() {
+        this.valor = valor;
+    }
+
+    public void setPadre() {
+        this.padre = padre;
+    }
+
+    public void setRaiz() {
+        this.raiz = raiz;
+    }
+
+    public void setHijos() {
+        this.hijos = hijos;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public Desencriptador_v1 getPadre() {
+        return padre;
+    }
+
+    public Desencriptador_v1 getRaiz() {
+        return raiz;
+    }
+
+    public ArrayList<Desencriptador_v1> getHijos() {
+        return hijos;
+    }
+
+
     public static void main(String[] args) {
 
         Scanner in = new Scanner(System.in);
@@ -14,52 +75,80 @@ public class Desencriptador_v1 {
         String nombre = in.next();
         System.out.print("Cadena de texto a buscar: ");
         String cadena = in.next();
-        File archivo = new File(nombre + ".mbx");
-        short[] busq = CadenaANumero(cadena);
-        short[] EDM = new short[65535];
-        int[] tmp = new int[(int) archivo.length()];
-        short[] datos=new short[(int)archivo.length()];
 
         try {
+            int pos = 0;
+            File archivo = new File(nombre + ".mbx");
+            short[] busq = CadenaANumero(cadena);
+            ArrayList<Short> EDM = new ArrayList<Short>();
+            int[] tmp = new int[(int) archivo.length()];
+            short[] datos = new short[(int) archivo.length()];
             FileInputStream sc = new FileInputStream(archivo);
             DataInputStream ec = new DataInputStream(sc);
             for (int i = 0; i < archivo.length(); i++) {
                 tmp[i] = ec.readUnsignedByte();
                 datos[i] = (short) tmp[i];
             }
-        for (int i = 0; i < 65535; i++) {
-            short[] copia = busq.clone();
-            ofuscar(copia, i);
-            String resultado=copia.toString();
-            EDM[i]=insertar(resultado,i);
+            for (int i = 0; i < 65535; i++) {
+                short[] copia = busq.clone();
+                ofuscar(copia, i);
+                EDM.add(insertar(copia, i));
+            }
+            for (int j = 0; j < archivo.length(); j++) {
+                if (buscar(EDM, cadena)) {
+                    short[] trozo = vec2str(datos, pos - 100, pos + 500, (int) archivo.length());
+                }
             }
         } catch (IOException e) {
             System.out.print("Error, no existe el archivo \n");
             System.exit(-1);
         }
 
-        /**
-         *
-         * Transforma la cadena a buscar en datos numericos
-         * @author dandevi
-         * @author dancres
-         * @param copia Copia de la cadena de texto
-         * @param i numero clave
-         *
-         */
     }
 
-    private static short insertar(String resultado, int i) {
-        short[] EDM=new short[65535];
-        EDM[i]=resultado.toCharArray();
+    public static boolean buscar(ArrayList<Short> EDM, String cadena) {
+
     }
 
     /**
-     * Transforma la cadena en valores numericos de tipo short
+     * Transformación de vector de short a String(mediante StringBuilder)
+     *
+     * @param vec      Vector de datos a tranformar
+     * @param ini      Posicion inicial
+     * @param fin      Posicion final
+     * @param longitud Longitud de datos
+     * @return Trozo de texto en formato de caracteres
+     */
+
+    static String vec2str(short[] vec, int ini, int fin, int longitud) { // Funcion que convierte un vector de short en un String(creada por el profesor)
+        StringBuilder res = new StringBuilder(fin - ini);
+        for (int i = ini; i < fin; i++) {
+            res.append((char) (vec[i] == 10 ? 10 : vec[i]));
+        }
+        return res.toString();
+    }
+
+    /**
+     * Transforma la cadena a buscar en datos numericos
+     *
+     * @param copia Copia de la cadena de texto
+     * @param i     numero clave
      * @author dandevi
      * @author dancres
+     */
+
+    private static short insertar(short[] copia, int i) {
+
+    }
+
+
+    /**
+     * Transforma la cadena en valores numericos de tipo short
+     *
      * @param cadena
      * @return busq2 cadena de texto pasada a numeros
+     * @author dandevi
+     * @author dancres
      */
 
     public static short[] CadenaANumero(String cadena) {
@@ -71,19 +160,20 @@ public class Desencriptador_v1 {
 
     /**
      * Ofusca la cadena de texto
-     * @author Cesar Vaca Rodriguez
+     *
      * @param copia
      * @param i
+     * @author Cesar Vaca Rodriguez
      */
 
     public static void ofuscar(short[] copia, int i) {
 
-        int w0,w1,b;
-        int[] lista=new int[copia.length];
-        for(int j=0;j<copia.length;j++){
-            lista[i]=copia[i];
+        int w0, w1, b;
+        int[] lista = new int[copia.length];
+        for (int j = 0; j < copia.length; j++) {
+            lista[i] = copia[i];
         }
-        int[] vPI = { 71, 241, 180, 230, 11, 106, 114, 72, 133, 78, 158, 235, 226, 248, 148, 83, 224, 187, 160, 2, 232,
+        int[] vPI = {71, 241, 180, 230, 11, 106, 114, 72, 133, 78, 158, 235, 226, 248, 148, 83, 224, 187, 160, 2, 232,
                 90, 9, 171, 219, 227, 186, 198, 124, 195, 16, 221, 57, 5, 150, 48, 245, 55, 96, 130, 140, 201, 19, 74,
                 107, 29, 243, 251, 143, 38, 151, 202, 145, 23, 1, 196, 50, 45, 110, 49, 149, 255, 217, 35, 209, 0, 94,
                 121, 220, 68, 59, 26, 40, 197, 97, 87, 32, 144, 61, 131, 185, 67, 190, 103, 210, 70, 66, 118, 192, 109,
@@ -94,9 +184,9 @@ public class Desencriptador_v1 {
                 252, 39, 174, 215, 189, 250, 7, 244, 204, 142, 95, 239, 53, 156, 132, 43, 21, 213, 119, 52, 73, 182, 18,
                 10, 127, 113, 136, 253, 157, 24, 65, 125, 147, 216, 88, 44, 206, 254, 36, 175, 222, 184, 54, 200, 161,
                 128, 166, 153, 152, 168, 47, 14, 129, 101, 115, 228, 194, 162, 138, 212, 225, 17, 208, 8, 139, 42, 242,
-                237, 154, 100, 63, 193, 108, 249, 236 };
+                237, 154, 100, 63, 193, 108, 249, 236};
 
-        int[] vPR = { 65, 54, 19, 98, 168, 33, 110, 187, 244, 22, 204, 4, 127, 100, 232, 93, 30, 242, 203, 42, 116, 197,
+        int[] vPR = {65, 54, 19, 98, 168, 33, 110, 187, 244, 22, 204, 4, 127, 100, 232, 93, 30, 242, 203, 42, 116, 197,
                 94, 53, 210, 149, 71, 158, 150, 45, 154, 136, 76, 125, 132, 63, 219, 172, 49, 182, 72, 95, 246, 196,
                 216, 57, 139, 231, 35, 59, 56, 142, 200, 193, 223, 37, 177, 32, 165, 70, 96, 78, 156, 251, 170, 211, 86,
                 81, 69, 124, 85, 0, 7, 201, 43, 157, 133, 155, 9, 160, 143, 173, 179, 15, 99, 171, 137, 75, 215, 167,
@@ -107,9 +197,9 @@ public class Desencriptador_v1 {
                 2, 166, 202, 105, 222, 80, 26, 17, 147, 185, 82, 135, 88, 252, 237, 29, 55, 73, 27, 106, 224, 41, 51,
                 153, 189, 108, 217, 148, 243, 64, 84, 111, 240, 198, 115, 184, 214, 62, 101, 24, 68, 31, 221, 103, 16,
                 241, 12, 25, 236, 174, 3, 161, 20, 123, 169, 11, 255, 248, 163, 192, 162, 1, 247, 46, 188, 36, 104, 117,
-                13, 254, 186, 47, 181, 208, 218, 61 };
+                13, 254, 186, 47, 181, 208, 218, 61};
 
-        int[] vPS = { 20, 83, 15, 86, 179, 200, 122, 156, 235, 101, 72, 23, 22, 21, 159, 2, 204, 84, 124, 131, 0, 13,
+        int[] vPS = {20, 83, 15, 86, 179, 200, 122, 156, 235, 101, 72, 23, 22, 21, 159, 2, 204, 84, 124, 131, 0, 13,
                 12, 11, 162, 98, 168, 118, 219, 217, 237, 199, 197, 164, 220, 172, 133, 116, 214, 208, 167, 155, 174,
                 154, 150, 113, 102, 195, 99, 153, 184, 221, 115, 146, 142, 132, 125, 165, 94, 209, 93, 147, 177, 87, 81,
                 80, 128, 137, 82, 148, 79, 78, 10, 107, 188, 141, 127, 110, 71, 70, 65, 64, 68, 1, 17, 203, 3, 63, 247,
@@ -120,7 +210,7 @@ public class Desencriptador_v1 {
                 140, 231, 229, 152, 50, 149, 211, 246, 74, 232, 166, 234, 233, 243, 213, 47, 112, 32, 242, 31, 5, 103,
                 173, 85, 16, 206, 205, 227, 39, 59, 218, 186, 215, 194, 38, 212, 145, 29, 210, 28, 34, 51, 248, 250,
                 241, 90, 239, 207, 144, 182, 139, 181, 189, 192, 191, 8, 151, 30, 108, 226, 97, 224, 198, 193, 89, 171,
-                187, 88, 222, 95, 223, 96, 121, 126, 178, 138 };
+                187, 88, 222, 95, 223, 96, 121, 126, 178, 138};
 
         for (int j = 0; j < (copia.length); j++) {
             w0 = i % 256;
